94311a8490e9e9d2af350a6389b55c855bceae04
olehmoishevych <olegmoishevych@gmail.com>
2025-02-11 22:18:41 +0200

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,15 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const PORT = 3000;
+
+  await app.listen(PORT, () => {
+    console.log(`🚀 Server started at port ${PORT}`);
+  });
+
+}
+
+bootstrap();
